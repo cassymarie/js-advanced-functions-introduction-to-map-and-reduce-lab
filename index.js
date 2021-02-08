ef4dc7e42870703f23894b7f0f315e8259810787
cassymarie <cassy2205@gmail.com>
2021-02-08 14:01:21 -0700

    commit tree d4f8b2474f73674ed77186fd9b1f887f89bafff1

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+// Map-like Functions
+function mapToNegativize(sourceArray){
+   return sourceArray.map(num => num * -1)
+}
+
+function mapToNoChange(sourceArray){
+    return sourceArray.map(num => num)
+}
+
+function mapToDouble(sourceArray){
+    return sourceArray.map(num => num * 2)
+}
+
+function mapToSquare(sourceArray){
+    return sourceArray.map(num => num * num)
+}
+
+// Reduce-like functions
+
+function reduceToTotal(sourceArray, startingPoint=0){
+    return sourceArray.reduce(function(total, item){ return total + item },startingPoint)
+}
+
+
+function reduceToAllTrue(sourceArray){
+    return sourceArray.reduce(function(total, item){ return total = true })
+}
+
+function reduceToAnyTrue(sourceArray){
+
+}
